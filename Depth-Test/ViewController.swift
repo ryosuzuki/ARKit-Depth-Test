4f6dd11b60a1c492e8558117855328a7a8d81655
Ryo Suzuki <ryosuzk@gmail.com>
2021-02-06 18:42:16 -0700

    Simplify the code

--- Depth-Test/ViewController.swift
+++ Depth-Test/ViewController.swift
@@ -11,9 +11,6 @@ import ARKit
 class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
-    @IBOutlet weak var hideMeshButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    @IBOutlet weak var planeDetectionButton: UIButton!
     
     let coachingOverlay = ARCoachingOverlayView()
     
@@ -64,138 +61,12 @@ class ViewController: UIViewController, ARSessionDelegate {
             let resultAnchor = AnchorEntity(world: result.worldTransform)
             resultAnchor.addChild(sphere(radius: 0.01, color: .lightGray))
 //            arView.scene.addAnchor(resultAnchor, removeAfter: 3)
-
-            /*
-            nearbyFaceWithClassification(to: result.worldTransform.position) { (centerOfFace, classification) in
-                // ...
-                DispatchQueue.main.async {
-                    // 4. Compute a position for the text which is near the result location, but offset 10 cm
-                    // towards the camera (along the ray) to minimize unintentional occlusions of the text by the mesh.
-                    let rayDirection = normalize(result.worldTransform.position - self.arView.cameraTransform.translation)
-                    let textPositionInWorldCoordinates = result.worldTransform.position - (rayDirection * 0.1)
-                    
-                    // 5. Create a 3D text to visualize the classification result.
-                    let textEntity = self.model(for: classification)
-
-                    // 6. Scale the text depending on the distance, such that it always appears with
-                    //    the same size on screen.
-                    let raycastDistance = distance(result.worldTransform.position, self.arView.cameraTransform.translation)
-                    textEntity.scale = .one * raycastDistance
-
-                    // 7. Place the text, facing the camera.
-                    var resultWithCameraOrientation = self.arView.cameraTransform
-                    resultWithCameraOrientation.translation = textPositionInWorldCoordinates
-                    let textAnchor = AnchorEntity(world: resultWithCameraOrientation.matrix)
-                    textAnchor.addChild(textEntity)
-                    self.arView.scene.addAnchor(textAnchor, removeAfter: 3)
-
-                    // 8. Visualize the center of the face (if any was found) for three seconds.
-                    //    It is possible that this is nil, e.g. if there was no face close enough to the tap location.
-                    if let centerOfFace = centerOfFace {
-                        let faceAnchor = AnchorEntity(world: centerOfFace)
-                        faceAnchor.addChild(self.sphere(radius: 0.01, color: classification.color))
-                        self.arView.scene.addAnchor(faceAnchor, removeAfter: 3)
-                    }
-                }
-            }
-            */
         }
     }
     
-    @IBAction func resetButtonPressed(_ sender: Any) {
-        if let configuration = arView.session.configuration {
-            arView.session.run(configuration, options: .resetSceneReconstruction)
-        }
-    }
-    
-    @IBAction func toggleMeshButtonPressed(_ button: UIButton) {
-        let isShowingMesh = arView.debugOptions.contains(.showSceneUnderstanding)
-        if isShowingMesh {
-            arView.debugOptions.remove(.showSceneUnderstanding)
-            button.setTitle("Show Mesh", for: [])
-        } else {
-            arView.debugOptions.insert(.showSceneUnderstanding)
-            button.setTitle("Hide Mesh", for: [])
-        }
-    }
-    
-    /// - Tag: TogglePlaneDetection
-    @IBAction func togglePlaneDetectionButtonPressed(_ button: UIButton) {
-        guard let configuration = arView.session.configuration as? ARWorldTrackingConfiguration else {
-            return
-        }
-        if configuration.planeDetection == [] {
-            configuration.planeDetection = [.horizontal, .vertical]
-            button.setTitle("Stop Plane Detection", for: [])
-        } else {
-            configuration.planeDetection = []
-            button.setTitle("Start Plane Detection", for: [])
-        }
-        arView.session.run(configuration)
-    }
-    
-    func nearbyFaceWithClassification(to location: SIMD3<Float>, completionBlock: @escaping (SIMD3<Float>?, ARMeshClassification) -> Void) {
-        guard let frame = arView.session.currentFrame else {
-            completionBlock(nil, .none)
-            return
-        }
-    
-        var meshAnchors = frame.anchors.compactMap({ $0 as? ARMeshAnchor })
-        
-        // Sort the mesh anchors by distance to the given location and filter out
-        // any anchors that are too far away (4 meters is a safe upper limit).
-        let cutoffDistance: Float = 4.0
-        meshAnchors.removeAll { distance($0.transform.position, location) > cutoffDistance }
-        meshAnchors.sort { distance($0.transform.position, location) < distance($1.transform.position, location) }
-
-        /*
-        DispatchQueue.global().async {
-            for anchor in meshAnchors {
-                for index in 0..<anchor.geometry.faces.count {
-                    // Get the center of the face so that we can compare it to the given location.
-                    let geometricCenterOfFace = anchor.geometry.centerOf(faceWithIndex: index)
-                    
-                    // Convert the face's center to world coordinates.
-                    var centerLocalTransform = matrix_identity_float4x4
-                    centerLocalTransform.columns.3 = SIMD4<Float>(geometricCenterOfFace.0, geometricCenterOfFace.1, geometricCenterOfFace.2, 1)
-                    let centerWorldPosition = (anchor.transform * centerLocalTransform).position
-                     
-                    // We're interested in a classification that is sufficiently close to the given location––within 5 cm.
-                    let distanceToFace = distance(centerWorldPosition, location)
-                    if distanceToFace <= 0.05 {
-                        // Get the semantic classification of the face and finish the search.
-                        let classification: ARMeshClassification = anchor.geometry.classificationOf(faceWithIndex: index)
-                        completionBlock(centerWorldPosition, classification)
-                        return
-                    }
-                }
-            }
-            
-            // Let the completion block know that no result was found.
-            completionBlock(nil, .none)
-        }
-         */
-    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         guard error is ARError else { return }
-        let errorWithInfo = error as NSError
-        let messages = [
-            errorWithInfo.localizedDescription,
-            errorWithInfo.localizedFailureReason,
-            errorWithInfo.localizedRecoverySuggestion
-        ]
-        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
-        DispatchQueue.main.async {
-            // Present an alert informing about the error that has occurred.
-            let alertController = UIAlertController(title: "The AR session failed.", message: errorMessage, preferredStyle: .alert)
-            let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
-                alertController.dismiss(animated: true, completion: nil)
-                self.resetButtonPressed(self)
-            }
-            alertController.addAction(restartAction)
-            self.present(alertController, animated: true, completion: nil)
-        }
     }
         
     func model(for classification: ARMeshClassification) -> ModelEntity {
